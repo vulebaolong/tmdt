d6bab62d6b7c3c862f0b78a23d1a1bf856b493c4
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-03-31 03:30:39 +0700

    99

--- src/lib/cloudinary-helper.ts
+++ src/lib/cloudinary-helper.ts
@@ -1,24 +1,25 @@
-import cloudinary from "./cloudinary";
+// import cloudinary from "./cloudinary";
 
 export async function uploadImageToCloudinary(formData: FormData) {
    const file = formData.get("file") as File;
    if (!file) return null;
 
-   const buffer = Buffer.from(await file.arrayBuffer());
+   // const buffer = Buffer.from(await file.arrayBuffer());
 
-   return new Promise<{ url: string; public_id: string }>((resolve, reject) => {
-      const stream = cloudinary.uploader.upload_stream({ folder: "products" }, (error, result) => {
-         if (error) return reject(error);
-         resolve({
-            url: result?.secure_url || "",
-            public_id: result?.public_id || "",
-         });
-      });
+   return new Promise<{ url: string; public_id: string }>(() => {
+      // const stream = cloudinary.uploader.upload_stream({ folder: "products" }, (error, result) => {
+      //    if (error) return reject(error);
+      //    resolve({
+      //       url: result?.secure_url || "",
+      //       public_id: result?.public_id || "",
+      //    });
+      // });
 
-      stream.end(buffer);
+      // stream.end(buffer);
    });
 }
 
 export async function deleteImageCloudinary(publicId: string) {
-   return cloudinary.uploader.destroy(publicId);
+   console.log(publicId);
+   // return cloudinary.uploader.destroy(publicId);
 }
