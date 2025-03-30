5fea089e27138de1454801da98ff52cabeb82048
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-03-31 03:01:53 +0700

    1

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,10 +1,9 @@
-import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_NAME } from "@/constant/app.constant";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: CLOUDINARY_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
