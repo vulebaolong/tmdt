06a801586c3498539661561e19538cba389d59eb
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-04-01 23:30:06 +0700

    am

--- src/components/provider/google/GoogleProvider.tsx
+++ src/components/provider/google/GoogleProvider.tsx
@@ -1,5 +1,7 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function GoogleProvider({ children }: { children: React.ReactNode }) {
-   return <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ``}>{children}</GoogleOAuthProvider>;
+   const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ``;
+   console.log({ "ở đây nà": CLIENT_ID });
+   return <GoogleOAuthProvider clientId={CLIENT_ID}>{children}</GoogleOAuthProvider>;
 }
