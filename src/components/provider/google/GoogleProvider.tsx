0216b71d8e80bfa24244d8c84404b754537959cd
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-04-02 00:35:11 +0700

    8

--- src/components/provider/google/GoogleProvider.tsx
+++ src/components/provider/google/GoogleProvider.tsx
@@ -1,9 +1,8 @@
 "use client";
 
+import { NEXT_PUBLIC_GOOGLE_CLIENT_ID } from "@/constant/app.constant";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function GoogleProvider({ children }: { children: React.ReactNode }) {
-   const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ``;
-   console.log({ "ở đây nà": CLIENT_ID, TEST_RUN_TIME: process?.env?.TEST_RUN_TIME });
-   return <GoogleOAuthProvider clientId={CLIENT_ID}>{children}</GoogleOAuthProvider>;
+   return <GoogleOAuthProvider clientId={NEXT_PUBLIC_GOOGLE_CLIENT_ID || ``}>{children}</GoogleOAuthProvider>;
 }
--- src/constant/app.constant.ts
+++ src/constant/app.constant.ts
@@ -4,8 +4,6 @@ export const ACCESS_TOKEN = "accessToken";
 export const REFRESH_TOKEN = "refreshToken";
 export const NEXT_LOCALE = "NEXT_LOCALE";
 
-export const BASE_DOMAIN_API = process.env.BASE_DOMAIN_API || `http://localhost:3069/`;
-// export const BASE_DOMAIN_API = process.env.NEXT_PUBLIC_BASE_DOMAIN_API || `https://be-node.vulebaolong.com/`;
 export const BASE_DOMAIN_CLOUDINARY = `https://res.cloudinary.com/vulebaolong/image/upload/`;
 export const FOLDER_IMAGE_BE = `images/`;
 
@@ -25,13 +23,10 @@ export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 
 export const NEXT_PUBLIC_GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-export const TEST_RUN_TIME = process.env.TEST_RUN_TIME;
 
 export const SHIPPING_FEE = 0;
 
 console.log({
-   BASE_DOMAIN_API,
-   BASE_DOMAIN_API_pro: process.env.NEXT_PUBLIC_BASE_DOMAIN_API,
    MONGODB_URI,
    ACCESS_TOKEN_SECRET,
    ACCESS_TOKEN_EXPIRES,
@@ -42,5 +37,4 @@ console.log({
    CLOUDINARY_API_SECRET,
    NEXT_PUBLIC_GOOGLE_CLIENT_ID,
    GOOGLE_CLIENT_SECRET,
-   TEST_RUN_TIME
 });
