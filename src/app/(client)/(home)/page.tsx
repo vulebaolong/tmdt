0b493eb2ee090bf6d2e8b4aa0647a043a7b9a28b
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-05-26 09:53:09 +0700

    1

--- src/app/(client)/(home)/page.tsx
+++ src/app/(client)/(home)/page.tsx
@@ -1,11 +1,12 @@
+import { getProductListAction2 } from "@/actions/product.action";
 import RootPage from "@/components/root-page/RootPage";
 import Home from "@/page/home/Home";
 
 export default async function page() {
-   // const products = await getProductListAction2({ page: 1, category: "0" });
+   const products = await getProductListAction2({ page: 1, category: "0" });
    return (
       <RootPage>
-         <Home  />
+         <Home products={products} />
       </RootPage>
    );
 }
--- src/app/bk/(client)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import ClientLayout from "@/layouts/client-layout/ClientLayout";
-
-type TProps = {
-   children: React.ReactNode;
-};
-
-export default function layout({ children }: TProps) {
-   return <ClientLayout>{children}</ClientLayout>;
-}
