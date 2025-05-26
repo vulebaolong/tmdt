6ded35ab1f6c817894673532930f9d1a3c4bfc35
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-05-26 09:39:15 +0700

    1

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -2,15 +2,13 @@ import About from "@/components/about/About";
 import Banner2 from "@/components/banner/Banner2";
 import ProductRoot from "@/components/product/product-root/ProductRoot";
 import Service2 from "@/components/service2/Service2";
-import { IProduct } from "@/schemas/product.schema";
-import { TResPagination } from "@/types/app.type";
 import { Stack } from "@mantine/core";
 
-type TProps = {
-   products?: TResPagination<IProduct>;
-};
+// type TProps = {
+//    products?: TResPagination<IProduct>;
+// };
 
-export default function Home({}: TProps) {
+export default function Home() {
    return (
       <Stack gap={50}>
          <Banner2 />
