839d110ae853e038eb587abb7b19684cd56dca36
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-05-29 17:49:41 +0700

    1

--- src/tantask/check-transaction.tanstack.ts
+++ src/tantask/check-transaction.tanstack.ts
@@ -8,7 +8,7 @@ import { useRef } from "react";
 export const useCheckTransaction = () => {
    const lastIdRef = useRef<string | undefined | null>(undefined);
    const info = useAppSelector((state) => state.user.info);
-   const isCheckTransaction = useAppSelector((state) => state.transaction.isCheckTransaction);
+   // const isCheckTransaction = useAppSelector((state) => state.transaction.isCheckTransaction);
    const dispatch = useAppDispatch();
    const queryClient = useQueryClient();
    const toast = useAppToast();
